bcd65d5dbdced03349ca698ad472d2406a928187
ssrawat1 <ssr911999@gmail.com>
2025-11-21 09:39:57 +0530

    Update the origin request

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(cookieParser(Secret_Key));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  const requestOrigin = req.headers.origin;
+  const requestOrigin = req.headers["origin"];
   const allowedOrigins = [Client_Url_1, Client_Url_2];
    console.log({requestOrigin,allowedOrigins})
   if (!allowedOrigins.includes(requestOrigin)) {
