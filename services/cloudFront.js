c4f4ffe4925f6137693830df1833e3bfdd8cd302
ssrawat1 <ssr911999@gmail.com>
2025-12-02 09:59:52 +0530

    Fix the access signed url bug

--- services/cloudFront.js
+++ services/cloudFront.js
@@ -2,12 +2,11 @@ import { getSignedUrl } from '@aws-sdk/cloudfront-signer';
 
 const privateKey = process.env.CLOUD_FRONT_PRIVATE_KEY;
 const keyPairId = process.env.CLOUDFRONT_KEY_PAIR_ID;
-const dateLessThan = new Date(Date.now() + 1000 * 60 * 60).toISOString(); // valid for 1 hr only
 
 const distributionName = process.env.CLOUDFRONT_DISTRIBUTION_URL;
 
 export const createCloudFrontGetSignedUrl = ({ key, action, filename }) => {
-  console.log({ dateLessThan });
+  const dateLessThan = new Date(Date.now() + 1000 * 60 * 60).toISOString(); // valid for 1 hr only
   const disposition = `${action === 'download' ? 'attachment' : 'inline'}; filename=${filename}`;
   const url = `${distributionName}/${key}?response-content-disposition=${encodeURIComponent(disposition)}`;
   const signedUrl = getSignedUrl({
@@ -16,6 +15,5 @@ export const createCloudFrontGetSignedUrl = ({ key, action, filename }) => {
     dateLessThan,
     privateKey,
   });
-  console.log({ signedUrl });
   return signedUrl;
 };
