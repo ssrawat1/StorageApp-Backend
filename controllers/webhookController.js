22b17a86edc36f10380c4004a2474a07f25ba013
ssrawat1 <ssr911999@gmail.com>
2025-12-04 00:18:34 +0530

    Update the github webhook signature logic'

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -47,6 +47,7 @@ export const handleRazorpayWebhook = async (req, res) => {
 export const handleGitHubWebhook = (req, res, next) => {
   const secret = process.env.GITHUB_WEBHOOK_SECRET;
   const header = req.headers['x-hub-signature-256'];
+  console.log({ secret, header });
   const payload = req.body;
   const isValidSignature = verifyGithubSignature(secret, header, payload);
 
