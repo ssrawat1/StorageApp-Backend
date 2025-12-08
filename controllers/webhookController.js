1dd82bd82ed721dc2435c8d89d5034d89a77d1bf
ssrawat1 <ssr911999@gmail.com>
2025-12-08 16:33:48 +0530

    Add the backend deployment logic inside the same webhook route so that i can dymaically trigger the deployment scripts automatically

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -93,7 +93,11 @@ export const handleGitHubWebhook = (req, res) => {
       ? '/home/ubuntu/deploy-frontend.sh'
       : '/home/ubuntu/deploy-backend.sh';
 
-  const bashChildProcess = spawn('bash', [scriptPath]);
+  const bashChildProcess = spawn('bash', [scriptPath], {
+    detached: true,
+    stdio: 'ignore', // child runs independently
+  });
+  bashChildProcess.unref();
 
   let logs = '';
 
