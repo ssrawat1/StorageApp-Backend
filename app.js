8d51ce8e375faffc3659398dab1acf88b55d59d8
ssrawat1 <ssr911999@gmail.com>
2025-12-09 14:52:51 +0530

    Debugging the github webhook error while deploying the backend

--- app.js
+++ app.js
@@ -106,9 +106,7 @@ app.post(
 app.use((err, req, res, next) => {
   // res.json(err);
   console.log({ hasSentHeader: res.headersSent });
-  if (res.headersSent) {
-    return;
-  }
+
   if (err.code === 11000) {
     return res.status(409).json({
       error: 'An account with this email already exists. Please log in to continue.',
