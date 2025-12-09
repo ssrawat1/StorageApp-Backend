9e90d4c8e9015d4477e7759da13a36ae180fe900
ssrawat1 <ssr911999@gmail.com>
2025-12-09 14:13:51 +0530

    Debugging the github webhook error while deploying the backend

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -78,9 +78,8 @@ export const handleGitHubWebhook = (req, res) => {
   console.log(`📧 Deployment triggered by: ${authorName} (${authorEmail})`);
 
   // Respond to GitHub immediately because github retry again and again if build and deploy process takes more time
-  res.status(200).json({
-    message: 'Webhook received. Deployment started. 🚀',
-  });
+  res.sendStatus(200);
+  console.log('Webhook received. Deployment started. 🚀');
 
   // ---- DEPLOYMENT SCRIPT RUNS HERE ---
 
