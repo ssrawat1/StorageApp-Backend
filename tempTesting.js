2da82f0269d788bfbbcf9619a499e7429f4fe663
ssrawat1 <ssr911999@gmail.com>
2025-12-09 14:36:58 +0530

    Debugging the github webhook error while deploying the backend

--- tempTesting.js
+++ tempTesting.js
@@ -1,14 +1,9 @@
- const allTestPassed = true;
-
-// Add color helpers
-const green = "\x1b[32m";
-const red = "\x1b[31m";
-const reset = "\x1b[0m";
+const allTestPassed = true;
 
 if (allTestPassed) {
-  console.log(`${green}✅ All backend tests have passed! 🚀${reset}`);
+  console.log(`✅ All backend tests have passed! 🚀`);
   process.exit(0);
 } else {
-  console.log(`${red}❌ Backend tests have failed! 🛑${reset}`);
+  console.log(`❌ Backend tests have failed! 🛑`);
   process.exit(1);
 }
